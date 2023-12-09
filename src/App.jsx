8e60d0fac9b48b5a96e86f086f977d45676960cd
Hamid207 <hamidmanafov@gmail.com>
2023-12-09 17:06:35 +0400

    create abot page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Layouts from "./components/layouts/Layouts";
 import HomePage from "./pages/home/HomePage";
 import "./assets/Global.css";
+import About from "./pages/about/About";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layouts />}>
           <Route index element={<HomePage />} />
+          <Route path="about" element={<About />} />
         </Route>
       </Routes>
     </>
---
+++ src/pages/about/About.jsx
@@ -0,0 +1,14 @@
+import PopularTopics from "../../components/popularTopics/PopularTopics";
+import EditorsPick from "../../components/editorsPick/EditorsPick";
+
+const About = () => {
+  return (
+    <>
+      <section>
+        <EditorsPick />
+      </section>
+    </>
+  );
+};
+
+export default About;
